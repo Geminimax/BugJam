eacdc96e83044176b72c4292c6a762ac66cc3d96
Marcelo Lemos <marceloharrylemos@gmail.com>
2021-11-12 10:25:32 -0300

    Fix player position when changing rooms

--- Scenes/LevelManager.gd
+++ Scenes/LevelManager.gd
@@ -1,40 +1,50 @@
 extends Node2D
 
 var player_inst
-var player_position
-var player_offset = Vector2(320, 180)
+var current_room
 
 var room_size = Vector2(640, 360)
-
-var player_scene = load("res://Scenes/Player.tscn")
+const PositionOffset = {CENTER = Vector2(320, 180), NORTH = Vector2(320, 60), SOUTH = Vector2(320, 300), WEST = Vector2(60, 180), EAST = Vector2(580, 180)}
 
 onready var level_generator = $LevelGenerator
 onready var camera = $Camera2D
+
+var player_scene = load("res://Scenes/Player.tscn")
 
 func _ready():
 	randomize()
 	$LevelGenerator.generate()
 	$LevelGenerator.instantiate_rooms(room_size, self, 'on_room_exit')
+	current_room = level_generator.rooms[0]
 	instantiate_player()
-	set_camera_position(level_generator.rooms[0])
+	set_camera_position(current_room)
 
 func instantiate_player():
 	player_inst = player_scene.instance()
 	add_child(player_inst)
-	set_player_position(level_generator.rooms[0])
+	set_player_position(current_room, PositionOffset.CENTER)
 
-func set_player_position(room):
-	player_position = room
-	player_inst.body.global_position = Vector2(player_position.x * room_size.x, player_position.y * room_size.y) + player_offset
+func set_player_position(room, offset):
+	player_inst.body.global_position = Vector2(current_room.x * room_size.x, current_room.y * room_size.y) + offset
+	print(room, offset)
 	
 func set_camera_position(room):
-	camera.global_position = Vector2(room.x * room_size.x, room.y * room_size.y) + player_offset
+	camera.global_position = Vector2(room.x * room_size.x, room.y * room_size.y) + PositionOffset.CENTER
 	
 func on_room_exit(dir):
-	print(player_position)
-	set_player_position(player_position+dir)
-	print(player_position)
-	set_camera_position(player_position)
+	current_room += dir
+	set_camera_position(current_room)
+	match dir:
+		Vector2.UP:
+			set_player_position(current_room, PositionOffset.SOUTH)
+		Vector2.DOWN:
+			set_player_position(current_room, PositionOffset.NORTH)
+		Vector2.LEFT:
+			set_player_position(current_room, PositionOffset.EAST)
+		Vector2.RIGHT:
+			set_player_position(current_room, PositionOffset.WEST)
+		_:
+			set_player_position(current_room, PositionOffset.CENTER)
 	
 	
 	
